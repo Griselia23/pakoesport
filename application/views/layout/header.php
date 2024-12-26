19de929631fe5899c123493a91c10a5f0799e79b
Griselia23 <panak675@gmail.com>
2024-12-26 13:22:09 +0700

    esport

---
+++ application/views/layout/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <title>Pako E-sport</title>
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta content="" name="keywords">
+  <meta content="" name="description">
+
+  <!-- Favicons -->
+  <link href="<?php echo base_url('application/template/img/favicon.jpeg'); ?>" rel="icon">
+  <link href="<?php echo base_url('application/template/img/apple-touch-icon.png'); ?>" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800" rel="stylesheet">
+
+  <!-- Bootstrap CSS File -->
+  <link href="<?php echo base_url('application/template/lib/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
+
+  <!-- Libraries CSS Files -->
+  <link href="<?php echo base_url('application/template/lib/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('application/template/lib/animate/animate.min.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('application/template/lib/venobox/venobox.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('application/template/lib/owlcarousel/assets/owl.carousel.min.css'); ?>" rel="stylesheet">
+
+  <!-- Main Stylesheet File -->
+  <link href="<?php echo base_url('application/template/css/style.css'); ?>" rel="stylesheet">
+
+</head>
+
+<body>
