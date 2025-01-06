a1112b763889398282a5eb77e6cfcb050a218ee2
Griselia23 <panak675@gmail.com>
2025-01-06 16:10:07 +0700

    dwada

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -9,20 +9,23 @@ class Admin_model extends CI_Model {
     }
 
     public function get_schedules_with_teams() {
-        $query = "INSERT INTO schedule (sections, match_number, division, start_date, team_1_id, team_2_id)
-    SELECT 
-        1 AS sections,
-        ROW_NUMBER() OVER (PARTITION BY r1.division ORDER BY r1.team, r2.team) AS match_number,
-        r1.division,
-        CURDATE() AS start_date,
-        r1.id AS team_1_id,
-        r2.id AS team_2_id
-    FROM 
-        register r1
-    JOIN 
-        register r2 ON r1.division = r2.division AND r1.id < r2.id
-    ORDER BY 
-        r1.division, r1.team, r2.team;";
+        $query = "SET @match_number = 0;
+
+INSERT INTO schedule (sections, match_number, division, start_date, team_1_id, team_2_id)
+SELECT 
+    1 AS sections,
+    @match_number := @match_number + 1 AS match_number,
+    r1.division,
+    CURDATE() AS start_date,
+    r1.id AS team_1_id,
+    r2.id AS team_2_id
+FROM 
+    register r1
+JOIN 
+    register r2 ON r1.division = r2.division AND r1.id < r2.id
+ORDER BY 
+    r1.division, r1.team, r2.team;
+";
     
         
         $this->db->query($query);
