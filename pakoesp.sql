03d58ac93a542de934299e442f6cefe30d838e4b
Griselia23 <panak675@gmail.com>
2025-01-07 14:00:40 +0700

    dadada

--- pakoesp.sql
+++ pakoesp.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 03 Jan 2025 pada 09.43
--- Versi server: 10.1.38-MariaDB
--- Versi PHP: 5.6.40
+-- Waktu pembuatan: 07 Jan 2025 pada 08.00
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 7.4.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,9 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `register` (
   `id` int(11) NOT NULL,
-  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creation_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `team` varchar(255) NOT NULL,
-  `points` int(11) NOT NULL DEFAULT '0',
+  `points` int(11) NOT NULL DEFAULT 0,
   `plant` varchar(255) NOT NULL,
   `leadernpk` varchar(50) NOT NULL,
   `leadername` varchar(255) NOT NULL,
@@ -56,7 +55,13 @@ CREATE TABLE `register` (
 
 INSERT INTO `register` (`id`, `creation_date`, `team`, `points`, `plant`, `leadernpk`, `leadername`, `number`, `member1npk`, `member1name`, `member2npk`, `member2name`, `member3npk`, `member3name`, `member4npk`, `member4name`, `member5npk`, `member5name`, `division`) VALUES
 (1, '2025-01-03 07:03:07', 'inko1', 0, 'inko', '218219', 'Bani', '239102931-4', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ml'),
-(2, '2025-01-03 07:03:27', 'assy1', 0, 'assy', '24123134', 'rizal', '2341412314', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ml');
+(2, '2025-01-03 07:03:27', 'assy1', 0, 'assy', '24123134', 'rizal', '2341412314', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ml'),
+(3, '2025-01-07 03:24:09', '2w', 0, 'inko', '291830103', 'anu', '391892301', '3137127419', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ml'),
+(4, '2025-01-07 04:26:38', 'fifa1', 0, 'inko', '313142', 'bani', '0293810', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'fifa'),
+(5, '2025-01-07 04:26:57', 'fifa2', 0, '2w', '371930190', 'riski', '31309131', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'fifa'),
+(6, '2025-01-07 04:34:05', 'fifa3', 0, 'inko', '231903810', 'hemmm', '32313', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'fifa'),
+(7, '2025-01-07 04:34:27', 'fifa4', 0, '2w', '29310301', 'hadi', '3180839', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'fifa'),
+(8, '2025-01-07 06:00:27', '4w', 0, '4w', '31441', 'hiihi', '3124231', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'ml');
 
 -- --------------------------------------------------------
 
@@ -66,9 +71,9 @@ INSERT INTO `register` (`id`, `creation_date`, `team`, `points`, `plant`, `leade
 
 CREATE TABLE `result` (
   `id` int(11) NOT NULL,
-  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creation_date` timestamp NOT NULL DEFAULT current_timestamp(),
   `matches` varchar(255) NOT NULL,
-  `points` int(11) DEFAULT '0',
+  `points` int(11) DEFAULT 0,
   `result` enum('win','lose','draw') NOT NULL,
   `upload` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -83,21 +88,16 @@ CREATE TABLE `schedule` (
   `id` int(11) NOT NULL,
   `match_number` int(11) NOT NULL,
   `division` enum('ml','fifa') NOT NULL,
-  `start_date` date NOT NULL,
-  `start_time` time NOT NULL,
-  `end_date` date NOT NULL,
-  `end_time` time NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `team_1_id` int(11) DEFAULT NULL,
-  `team_2_id` int(11) DEFAULT NULL
+  `start_date` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `schedule`
 --
 
-INSERT INTO `schedule` (`id`, `match_number`, `division`, `start_date`, `start_time`, `end_date`, `end_time`, `created_at`, `team_1_id`, `team_2_id`) VALUES
-(1, 1, 'ml', '2025-01-03', '22:00:00', '2025-01-03', '23:00:00', '2025-01-03 07:03:52', 1, 2);
+INSERT INTO `schedule` (`id`, `match_number`, `division`, `start_date`) VALUES
+(5, 1, 'fifa', '2025-01-07'),
+(7, 1, 'ml', '2025-01-10');
 
 -- --------------------------------------------------------
 
@@ -110,7 +110,7 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `npk` varchar(50) NOT NULL,
-  `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `creation_date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -157,7 +157,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `register`
 --
 ALTER TABLE `register`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `result`
@@ -169,7 +169,7 @@ ALTER TABLE `result`
 -- AUTO_INCREMENT untuk tabel `schedule`
 --
 ALTER TABLE `schedule`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
