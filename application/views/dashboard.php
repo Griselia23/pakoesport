1f4ba480d2ff7e8cd64193ea9ce4d2f6113f4099
Griselia23 <panak675@gmail.com>
2025-01-14 15:26:37 +0700

    nih

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -481,37 +481,36 @@
             </li>
 
             <li>
-              <a data-toggle="collapse" href="#faq3" class="collapsed">Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi? <i class="fa fa-minus-circle"></i></a>
+              <a data-toggle="collapse" href="#faq3" class="collapsed">Ketauan licik dalam upload hasil akan diberikan sanksi sesuai pelanggaran!<i class="fa fa-minus-circle"></i></a>
               <div id="faq3" class="collapse" data-parent="#faq-list">
                 <p>
-                  Eleifend mi in nulla posuere sollicitudin aliquam ultrices sagittis orci. Faucibus pulvinar elementum integer enim. Sem nulla pharetra diam sit amet nisl suscipit. Rutrum tellus pellentesque eu tincidunt. Lectus urna duis convallis convallis tellus. Urna molestie at elementum eu facilisis sed odio morbi quis
+                  Bijak terhadap upload hasil karena kami percaya kepada anda.
                 </p>
               </div>
             </li>
 
             <li>
-              <a data-toggle="collapse" href="#faq4" class="collapsed">Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla? <i class="fa fa-minus-circle"></i></a>
+              <a data-toggle="collapse" href="#faq4" class="collapsed">Score ML dan FIFA berbeda!<i class="fa fa-minus-circle"></i></a>
               <div id="faq4" class="collapse" data-parent="#faq-list">
                 <p>
-                  Dolor sit amet consectetur adipiscing elit pellentesque habitant morbi. Id interdum velit laoreet id donec ultrices. Fringilla phasellus faucibus scelerisque eleifend donec pretium. Est pellentesque elit ullamcorper dignissim. Mauris ultrices eros in cursus turpis massa tincidunt dui.
+                  FIFA menang akan mendapat 3 points kalah 0 dan draw 1, ML menang akan mendapatkan point dari banyak nya kemenangan.
                 </p>
               </div>
             </li>
 
             <li>
-              <a data-toggle="collapse" href="#faq5" class="collapsed">Tempus quam pellentesque nec nam aliquam sem et tortor consequat? <i class="fa fa-minus-circle"></i></a>
+              <a data-toggle="collapse" href="#faq5" class="collapsed">Lupa password!<i class="fa fa-minus-circle"></i></a>
               <div id="faq5" class="collapse" data-parent="#faq-list">
                 <p>
-                  Molestie a iaculis at erat pellentesque adipiscing commodo. Dignissim suspendisse in est ante in. Nunc vel risus commodo viverra maecenas accumsan. Sit amet nisl suscipit adipiscing bibendum est. Purus gravida quis blandit turpis cursus in
-                </p>
+                  Minta panitia untuk berikan password baru sesuai nama tim dan npk leader.
               </div>
             </li>
 
             <li>
-              <a data-toggle="collapse" href="#faq6" class="collapsed">Tortor vitae purus faucibus ornare. Varius vel pharetra vel turpis nunc eget lorem dolor? <i class="fa fa-minus-circle"></i></a>
+              <a data-toggle="collapse" href="#faq6" class="collapsed">Dilarang melontarkan kalimat toxic atau menyangkut rasisme antar tim!<i class="fa fa-minus-circle"></i></a>
               <div id="faq6" class="collapse" data-parent="#faq-list">
                 <p>
-                  Laoreet sit amet cursus sit amet dictum sit amet justo. Mauris vitae ultricies leo integer malesuada nunc vel. Tincidunt eget nullam non nisi est sit amet. Turpis nunc eget lorem dolor sed. Ut venenatis tellus in metus vulputate eu scelerisque. Pellentesque diam volutpat commodo sed egestas egestas fringilla phasellus faucibus. Nibh tellus molestie nunc non blandit massa enim nec.
+                 Raih juaramu tanpa menyakiti team lain!
                 </p>
               </div>
             </li>
--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -70,13 +70,13 @@
 
 <!-- DataTables JavaScript Files -->
 <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<!-- <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script> -->
 
   <!-- Contact Form JavaScript File -->
   <script src="<?php echo base_url('application/template/contactform/contactform.js'); ?>"></script>
 
   <!-- application/template Main Javascript File -->
   <script src="<?php echo base_url('application/template/js/main.js'); ?>"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
 </body>
 </html>
